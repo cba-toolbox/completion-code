1ba445fc3593250d46536cca6ac3dab1c614cc61
award-psych <119397283+award-psych@users.noreply.github.com>
2023-01-04 20:25:41 +0900

    add data for id

--- completion-code/task.js
+++ completion-code/task.js
@@ -6,6 +6,7 @@ var finish = {
   type: jsPsychHtmlButtonResponse,
   stimulus: '<p style="font-size:30px;">研究に参加いただき，誠にありがとうございました。</p><p style="font-size:30px;">これですべての課題とアンケートが終了しました。</p><p style="font-size:30px;">あなたの参加IDは，<font color=”#6495ED”>  '+ id_no +' </font>です</p><p style="font-size:20px;">紙にメモし，参加IDをコピーしてから，以下の「終了」をクリックして終了ください</p>',
   choices: ['終了'],
+  data: {id: id_no}
 };
 
 /*タイムラインの設定*/
